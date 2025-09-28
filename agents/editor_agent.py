c70bcf44cba68eca421836f4bdfa4388a38c614b
Daniel Parsons <parsonsdwp@gmail.com>
2025-09-29 08:45:28 +0900

    feat: create agent classes

---
+++ agents/editor_agent.py
@@ -0,0 +1,9 @@
+class EditorAgent:
+    """
+    This agent receives instructions from other agents that help it to edit the given paragraph.
+    """
+    def __init__(self):
+        """
+        Initialize the editor agent
+        """
+        print("Initialize the editor agent")
